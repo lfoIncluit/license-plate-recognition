859a3b614ff8f4922a9d9c0a010f2260c534667f
Luciano Fernandez Oria <luciano.fernandez@incluit.com>
2021-11-03 15:44:39 -0300

    [007] pd detection updated.

--- testOpenVINO.py
+++ testOpenVINO.py
@@ -4,6 +4,8 @@ from openvino.inference_engine import IECore
 from imutils import paths
 
 TEST_PATH = "testImgs"
+CHINA = False
+CONF = 0.4
 
 pColor = (0, 0, 255)  # plate bounding-rect and information color
 rectThinkness = 2
@@ -81,8 +83,12 @@ items = [
     "Z",
 ]
 
+pd_model_xml = "./vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.xml"
+pd_model_bin = "./vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.bin"
+
 lpr_model_xml = "./license-plate-recognition-barrier-0007/license-plate-recognition-barrier-0007.xml"
 lpr_model_bin = "./license-plate-recognition-barrier-0007/license-plate-recognition-barrier-0007.bin"
+
 device = "CPU"
 
 def drawText(frame, scale, rectX, rectY, rectColor, text):
@@ -95,74 +101,67 @@ def drawText(frame, scale, rectX, rectY, rectColor, text):
         frame, text, (rectX, top), cv2.FONT_HERSHEY_SIMPLEX, scale, rectColor, 3
     )
 
-def plateRecognition(frame, pd_net, lpr_execution_net, lpr_input_blob):
+def plateRecognition(frame, pd_execution_net, pd_input_blob, lpr_execution_net, lpr_input_blob):
 
     frame_width = frame.shape[1]
     frame_height = frame.shape[0]
-    # name: "input" , shape: [1x3x300x300] - An input image in the format [BxCxHxW], where:
-    pd_blob = cv2.dnn.blobFromImage(frame, size=(300, 300), ddepth=cv2.CV_8U)
-    pd_net.setInput(pd_blob)
-    out_pb = pd_net.forward()
-    global cur_request_id
 
-    # An every detection is a vector [imageId, classId, conf, x, y, X, Y]
-    for detection in out_pb.reshape(-1, 7):
+    pd_blob = cv2.dnn.blobFromImage(frame, size=(300, 300), ddepth=cv2.CV_8U)
+    pd_results = pd_execution_net.infer( inputs={pd_input_blob: pd_blob}).get("DetectionOutput_")
+    
+    for detection in pd_results[0][0]:
         conf = detection[2]
-        if conf < 0.4:
+        if conf < CONF:
             continue
 
         classId = int(detection[1])
-        
+        xmin = int(detection[3] * frame_width)
+        ymin = int(detection[4] * frame_height)
+        xmax = int(detection[5] * frame_width)
+        ymax = int(detection[6] * frame_height)
+        xmin = max(0, xmin - 5)
+        ymin = max(0, ymin - 5)
+        xmax = min(xmax + 5, frame_width - 1)
+        ymax = min(ymax + 5, frame_height - 1)
+  
         if classId == 2:  # plate
-            xmin = int(detection[3] * frame_width)
-            ymin = int(detection[4] * frame_height)
-            xmax = int(detection[5] * frame_width)
-            ymax = int(detection[6] * frame_height)
-
-            xmin = max(0, xmin - 10)
-            ymin = max(0, ymin - 10)
-            xmax = min(xmax + 10, frame_width - 1)
-            ymax = min(ymax + 10, frame_height - 1)
-
             rectW = xmax - xmin
-            if rectW < 93:  # Minimal weight in plate-recognition-barrier-0001 is 94
-                continue
+            if rectW > 93:  # Minimal weight in plate-recognition-barrier-0001 is 94
+                # Crop a license plate. Do some offsets to better fit a plate.
+                lpImg = frame[ymin : ymax + 1, xmin : xmax + 1]
+                blob = cv2.dnn.blobFromImage(lpImg, size=(94, 24), ddepth=cv2.CV_8U)
+                lpr_results = lpr_execution_net.infer( inputs={lpr_input_blob: blob}).get("d_predictions.0")
 
-            # Crop a license plate. Do some offsets to better fit a plate.
-            lpImg = frame[ymin : ymax + 1, xmin : xmax + 1]
-            blob = cv2.dnn.blobFromImage(lpImg, size=(94, 24), ddepth=cv2.CV_8U)
-            results = lpr_execution_net.infer( inputs={lpr_input_blob: blob}).get("d_predictions.0")
-        
-            content = ""
-            for _ in results[0]:
-                if _ == -1:
-                    break
-                content += items[_]
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), pColor, rectThinkness)
-            drawText(frame, rectW * 0.008, xmin, ymin, pColor, content)
-        else:
-            continue
+                content = ""
+                for _ in lpr_results[0]:
+                    if _ == -1:
+                        break
+                    elif not CHINA and 10 <= _ <=43:
+                        pass
+                    else:
+                        content += items[_]
+                drawText(frame, rectW * 0.008, xmin, ymin, pColor, content)
+
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), pColor, rectThinkness)
 
     showImg = imutils.resize(frame, height=600)
     cv2.imshow("showImg", showImg)
 
 def main():
 
-    # https://docs.openvinotoolkit.org/2019_R1/_vehicle_license_plate_detection_barrier_0106_description_vehicle_license_plate_detection_barrier_0106.html
-    pd_net = cv2.dnn.readNet(
-        "./vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.xml",
-        "./vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.bin",
-    )
-    pd_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
-    pd_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
     ie = IECore()
+    
+    pd_neural_net = ie.read_network(model=pd_model_xml, weights=pd_model_bin)
+    if pd_neural_net is not None:
+        pd_input_blob = next(iter(pd_neural_net.input_info))
+        pd_neural_net.batch_size = 1
+        pd_execution_net = ie.load_network(network=pd_neural_net, device_name=device.upper())
+
     lpr_neural_net = ie.read_network(model=lpr_model_xml, weights=lpr_model_bin)
     if lpr_neural_net is not None:
         lpr_input_blob = next(iter(lpr_neural_net.input_info))
         lpr_neural_net.batch_size = 1
         lpr_execution_net = ie.load_network(network=lpr_neural_net, device_name=device.upper())
-    n, c, h, w = lpr_neural_net.input_info[lpr_input_blob].input_data.shape
     
     for imagePath in paths.list_images(TEST_PATH):
         print(imagePath)
@@ -170,7 +169,7 @@ def main():
         if img is None:
             continue
 
-        plateRecognition(img, pd_net, lpr_execution_net, lpr_input_blob)
+        plateRecognition(img, pd_execution_net, pd_input_blob, lpr_execution_net, lpr_input_blob)
         cv2.waitKey(0)
 
 if __name__ == "__main__":
